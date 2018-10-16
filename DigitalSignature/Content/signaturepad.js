ec60ac190cd21072557d8c4b94b67b6bc3bf12c4
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-16 21:19:53 +0500

    Update the Signature js file

---
+++ DigitalSignature/Content/signaturepad.js
@@ -0,0 +1,28 @@
+﻿
+$(document).ready(function () {
+
+    var option = {
+        defaultAction: "drawIt",
+        drawOnly: true,
+        lineTop: 70,
+        lineMargin: 20,
+        penColor: '#fff'
+
+    }
+    $('.sig-form').signaturePad(option);
+    var signature = $('.signed').signaturePad({ displayOnly: true });
+
+
+    $('#get').click(function () {
+        $.ajax({
+            url: "Home/index",
+            type: 'POST',
+            success: function (data) {
+                signature.regenerate(data);
+            }
+
+        });
+    });
+
+
+});
